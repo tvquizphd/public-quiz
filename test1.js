acd13be03bb022f51518ce88661fffcd01a0bad7
TVQuizPhD <tvquizphd@gmail.com>
2023-01-23 12:02:04 -0500

    fix test script

--- test1.js
+++ test1.js
@@ -27,7 +27,7 @@ const main = async (secret) => {
     owner_token: process.argv[2] || ""
   };
   const name = 'TEMP_OUT';
-  const text = process.argv[3] "";
+  const text = process.argv[3] || "";
   const get_api = `/repos/${git.owner}/${git.repo}`;
   const authorization = `token ${git.owner_token}`;
   const get_r = await request(`GET ${get_api}`, {
