645c1ac7ce882487f0da7c15ca2e87bc33304bfa
TVQuizPhD <tvquizphd@gmail.com>
2023-02-02 09:37:26 -0500

    attempt to fix actions issues

--- types/util/types.d.ts
+++ types/util/types.d.ts
@@ -1,12 +1,14 @@
-export declare type Trio = [string, string, string];
+export declare type Five = [string, string, string, string, string];
 export declare type Quad = [string, string, string, string];
+export declare type Trio = [string, string, string];
 export declare type Duo = [string, string];
 export declare type Git = {
     repo: string;
     owner: string;
     owner_token: string;
 };
+declare function isFive(args: string[]): args is Five;
 declare function isQuad(args: string[]): args is Quad;
 declare function isTrio(args: string[]): args is Trio;
 declare function isDuo(args: string[]): args is Duo;
-export { isQuad, isTrio, isDuo };
+export { isQuad, isTrio, isDuo, isFive };
