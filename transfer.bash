ebe212624beb1b3fd81fc8c30dee3df2cefb3aa2
TV Quiz PhD <75504552+tvquizphd@users.noreply.github.com>
2023-02-09 22:21:37 -0500

    Delete transfer.bash

--- transfer.bash
+++
@@ -1,3 +0,0 @@
-NO_CACHE='Cache-Control: no-cache'
-curl -H "$NO_CACHE" -o docs/environment.csv https://pass.tvquizphd.com/environment.csv
-curl -H "$NO_CACHE" -o docs/pub.txt https://pass.tvquizphd.com/pub.txt
